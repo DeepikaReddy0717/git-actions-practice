51a4ba21dc507e5aab366eccdc9e94f5ae6dc4d6
DeepikaReddy <deepikareddymandapati@gmail.com>
2025-10-23 10:46:03 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        Hello good Morning i am preparing for the CI/CD Lab Exam
+      </div>
+      
+    </>
+  )
+}
+
+export default App
